ea46747f1d54390a6278866374b4671828eb2a0d
Ivan <metaldaze@mail.ru>
2016-05-13 18:43:18 +0300

    2сохранение

--- app/src/main/java/ru/brainworkout/sandow_gym/DatabaseHandler.java
+++ app/src/main/java/ru/brainworkout/sandow_gym/DatabaseHandler.java
@@ -18,7 +18,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     // Database Name
-    private static final String DATABASE_NAME = "trainingManager";
+    private static final String DATABASE_NAME = "trainingCalendar";
 
     // Contacts table name
     private static final String TABLE_EXERCISES = "exercises";
